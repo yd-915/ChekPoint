d37f7bbf25c0d39831fa9c7671b8cedfce8ae45d
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-09-18 20:47:32 +0200

    Added more starter files and nav routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,19 @@ import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import Homepage from "./pages/Homepage";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/"
-          element={<Homepage />}
-        />
-        <Route
-          path="product"
-          element={<Product />}
-        />
-        <Route
-          path="pricing"
-          element={<Pricing />}
-        />
-        <Route
-          path="app"
-          element={<AppLayout />}
-        />
-        <Route
-          path="*"
-          element={<PageNotFound />}
-        />
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
   );
