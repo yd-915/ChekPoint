6e474cb35c9c16dde99c06e75be8226f97f26e33
Gjon Hajdari <gjonhajdari1@gmail.com>
2023-09-18 21:01:26 +0200

    Created static layout for app route

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,18 @@
+import AppNav from "./AppNav";
+import Logo from "./Logo";
+import Footer from "./Footer";
+import styles from "./SideBar.module.css";
+
+function SideBar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+
+      <p>List of cities</p>
+      <Footer />
+    </div>
+  );
+}
+
+export default SideBar;
