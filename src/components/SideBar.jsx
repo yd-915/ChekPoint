60126431409cb12863f41b54b2e15958a8801828
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2024-02-25 01:11:20 -0500

    Update SideBar.jsx

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -6,13 +6,13 @@ import { Outlet } from "react-router-dom";
 function SideBar() {
   const styles = {
    sidebar: {
-  flexBasis: 56rem,
+  flexBasis: 56 rem,
   backgroundColor: var(--color-dark--1),
-  padding: 3rem 5rem 3.5rem 5rem,
+  padding: 3 rem 5 rem 3.5 rem 5 rem,
   display: flex,
   flexDirection: column,
   alignItems: center,
-  height: calc(100vh - 4.8rem),
+  height: calc(100vh - 4.8 rem),
 }
   };
 
