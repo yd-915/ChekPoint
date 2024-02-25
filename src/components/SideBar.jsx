a08a6ceeeec670104db5949ae0b48f1b8779664c
Yonis Dideh <74731447+yd-915@users.noreply.github.com>
2024-02-25 01:04:42 -0500

    Update SideBar.jsx

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -9,7 +9,6 @@ function SideBar() {
   flex-basis: 56rem;
   background-color: var(--color-dark--1);
   padding: 3rem 5rem 3.5rem 5rem;
-
   display: flex;
   flex-direction: column;
   align-items: center;
